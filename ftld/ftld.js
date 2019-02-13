db6743821cd8a22392c99a40c39af9cb3c8719cc
fateleak <bingfeng.ch3n@gmail.com>
2019-02-13 17:34:16 +0800

    op

---
+++ ftld/ftld.js
@@ -0,0 +1,19 @@
+
+
+const whois = require('whois')
+
+
+function check_domain(domain, callback) {
+    console.log('checking', domain)
+    whois.lookup(domain, function (err, data) {
+        let ava = true 
+        if (data && data.includes('xpiration')) {
+            ava = false
+        }
+        console.log(ava? 'AA': 'NA', domain, data?data.substring(0, 100):'empty')
+        callback(ava, domain, data)
+    })
+}
+
+
+exports.check_domain = check_domain
--- ftld/index.js
+++ ftld/index.js
@@ -10,6 +10,7 @@ const store = new Store()
 const urllib = require('url')
 const fs = require('fs')
 const databind = require('./databind')
+const whois = require('whois')
 
 if (utils.is_cn) {
   moment.locale('zh-cn')
@@ -27,6 +28,35 @@ function open_win(win_name) {
   electron.ipcRenderer.send('open-win', win_name)
 }
 
+const ftld = require('./ftld')
 function on_click_execute() {
   console.log('exec')
+
+  let pre = $('#prefix_data').val()
+  let post = $('#postfix_data').val()
+  let tlds = $('#tld_data').val()
+  pre = pre.split('\n')
+  post = post.split('\n')
+  tlds = tlds.split('\n')
+
+  console.log(pre, post, tlds)
+  pre.forEach(a => {
+    post.forEach(b=>{
+      tlds.forEach(c=>{
+        let dm = `${a}${b}${c}`
+        ftld.check_domain(dm, on_domain_result)
+      })
+    })
+  });
+
+}
+
+function on_domain_result(is_avaliable, domain, whois) {
+  
+  let ele_text = `<span>${is_avaliable?'AA': 'NA'} ${domain}  ${whois?whois.substring(0, 20).trim():'empty'}
+  </span>
+  `
+  if (is_avaliable) {
+  $('#result_area').append($(ele_text))
+  }
 }
